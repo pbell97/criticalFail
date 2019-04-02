fd057a9cd49e72a8e2981b7bf096b2e71efdb697
Christian Donoho <46464951+ChDonoho@users.noreply.github.com>
2019-04-01 20:44:44 -0500

    Update SQL-Python Initializer Run.py

--- SQL-Python Initializer Run.py
+++ SQL-Python Initializer Run.py
@@ -1,6 +1,14 @@
-##Make sure you add your password to the connector parameters
-##Have MySQL insalled, Python 3.6, and python mysql connector
-##MySQL connector only works with python 3.6 or below
+#Make sure you add your password to the connector parameters
+#Have MySQL insalled, Python 3.6, and python mysql connector
+#MySQL connector only works with python 3.6 or below
+
+#Questions
+#is the token primary key in cf_users an int or string?
+#what would be a reasonable length for usernames in all tables?
+#Is color in cf_messages a string or HEX?
+#Using bit to represent boolean in messages for the GMflag (0 = false, 1 = true)
+#What is the message limit in cf_messages?
+
 
 import mysql.connector
 
@@ -11,23 +19,42 @@ cnx = mysql.connector.connect(
 
 mycursor = cnx.cursor()
 
-##CODE THAT PURGES ALL DATABASES
+#Creates database criticalFail
+mycursor.execute("CREATE DATABASE IF NOT EXISTS criticalFail")
 
-##Creates database criticalFail
-mycursor.execute("CREATE DATABASE testdb1")
+#Creates table queries
+"""
+init_cf_tokens = "CREATE TABLE IF NOT EXISTS cf_tokens (token INT AUTO_INCREMENT NOT NULL, username VARCHAR(30), expiration DATE, PRIMARY KEY(token));"
+init_cf_campaigns = "CREATE TABLE IF NOT EXISTS cf_campaigns (campaignID INT AUTO_INCREMENT NOT NULL, GMname VARCHAR(30), PRIMARY KEY(campaignID));"
+init_cf_users = "CREATE TABLE IF NOT EXISTS cf_users (campaignID INT, username VARCHAR(30), password VARCHAR(128), color VARCHAR(30), attributes VARCHAR(256), GMflag INT, PRIMARY KEY(campaignID, username), FOREIGN KEY(campaignID) REFERENCES cf_campaigns(campaignID));"
+init_cf_messages = "CREATE TABLE IF NOT EXISTS cf_messages (campaignID INT, messageID INT AUTO_INCREMENT NOT NULL, message __________, username VARCHAR(30), time TIME, recipient VARCHAR(30), PRIMARY KEY(campaignID, messageID) FOREIGN KEY(campaignID) REFERENCES cf_campaigns(campaignID));"
+"""
 
-##Creates tables
-init_cf_users = "CREATE TABLE cf_users (ATTRIBUTES)"
-init_cf_campaigns = ""
-init_cf_messages = ""
-init_cf_tokens = ""
+#
+"""
+mycursor.execute(init_cf_tokens)
+mycursor.execute(init_cf_campaigns)
+mycursor.execute(init_cf_users)
+mycursor.execute(init_cf_messages)
+"""
 
-##mycursor.execute(init_cf_users, init_cf_campaigns, init_cf_messages, init_cf_tokens)
-##mycursor.execute(init_cf_users)
-##mycursor.execute(init_cf_users)
-##mycursor.execute(init_cf_users)
-##mycursor.execute(init_cf_users)
+"""
+Series of test inserts
 
+tokensTest = "INSERT INTO cf_tokens (username, expiration) VALUES ("Joe", 1999-04-01);"
+campaignsTest = "INSERT INTO cf_campaigns (GMname) VALUES ("Phil");"
+usersTest = "INSERT INTO cf_users (username, password, color, attributes, GMflag) VALUES ("Fred", "asdfavxdefasdf", "Blue", "testattribute", 0);"
+messagesTest = "INSERT INTO cf_messages(message, username, time, recipient) VALUES ("Hello", "Joe", 19:00:00, "Fred");"
+
+mycursor.execute(tokensTest)
+mycursor.execute(campaignsTest)
+mycursor.execute(usersTest)
+mycursor.execute(messagesTest)
+
+
+""" 
+
+    
 ##myresult = mycursor.fetchall()
 
 #==========================================================
