aba8eeb0c3a62d417df7807985f4e627d3d04e3d
Peyton Johnson <willjohnsonk@gmail.com>
2019-04-03 18:27:07 -0500

    Updated Observer Page

---
+++ observerPage.js
@@ -0,0 +1,8 @@
+request = new XMLHttpRequest()
+request.onreadystatechange = function(){
+    if(request.readyState === 4 && request.status === 200){
+         console.log(this.responseText);
+    }
+}
+request.open("GET", "http://ec2-3-208-34-201.compute-1.amazonaws.com:80/serverInfo/", true);
+request.send();
