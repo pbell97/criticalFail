8186eed892fd0e39a90a913df527bcc7182c3c75
pbell97 <patrickb97@hotmail.com>
2019-04-03 03:50:34 +0000

    Made a few small changes to sql initializer to get working on server

--- SQL-Python Initializer Run.py
+++ SQL-Python Initializer Run.py
@@ -9,48 +9,53 @@
 #Using bit to represent boolean in messages for the GMflag (0 = false, 1 = true)
 #What is the message limit in cf_messages?
 
+#Probably need to change message time to DATE. Does 'DATE' include the time too?
 
 import mysql.connector
 
+print("Signing in to database...")
 cnx = mysql.connector.connect(
     user='root',
-    password='',
+    password='criticalFail',
+    database='criticalFail',
     host='localhost')
 
 mycursor = cnx.cursor()
 
 #Creates database criticalFail
-mycursor.execute("CREATE DATABASE IF NOT EXISTS criticalFail")
+print("Creating Database...")
+mycursor.execute("CREATE DATABASE IF NOT EXISTS criticalFail;")
+mycursor.execute("USE criticalfail;")
+
 
 #Creates table queries
-"""
-init_cf_tokens = "CREATE TABLE IF NOT EXISTS cf_tokens (token INT AUTO_INCREMENT NOT NULL, username VARCHAR(30), expiration DATE, PRIMARY KEY(token));"
+init_cf_tokens = "CREATE TABLE IF NOT EXISTS cf_tokens (token VARCHAR(10) NOT NULL, username VARCHAR(30), expiration DATE, PRIMARY KEY(token));"
 init_cf_campaigns = "CREATE TABLE IF NOT EXISTS cf_campaigns (campaignID INT AUTO_INCREMENT NOT NULL, GMname VARCHAR(30), PRIMARY KEY(campaignID));"
 init_cf_users = "CREATE TABLE IF NOT EXISTS cf_users (campaignID INT, username VARCHAR(30), password VARCHAR(128), color VARCHAR(30), attributes VARCHAR(256), GMflag INT, PRIMARY KEY(campaignID, username), FOREIGN KEY(campaignID) REFERENCES cf_campaigns(campaignID));"
-init_cf_messages = "CREATE TABLE IF NOT EXISTS cf_messages (campaignID INT, messageID INT AUTO_INCREMENT NOT NULL, message __________, username VARCHAR(30), time TIME, recipient VARCHAR(30), PRIMARY KEY(campaignID, messageID) FOREIGN KEY(campaignID) REFERENCES cf_campaigns(campaignID));"
-"""
+init_cf_messages = "CREATE TABLE IF NOT EXISTS cf_messages (campaignID INT, messageID INT NOT NULL, message VARCHAR(250), username VARCHAR(30), time TIME, recipient VARCHAR(30), PRIMARY KEY(campaignID, messageID), FOREIGN KEY(campaignID) REFERENCES cf_campaigns(campaignID));"
 
-#
-"""
+
+
+print("Creating Tables...")
 mycursor.execute(init_cf_tokens)
 mycursor.execute(init_cf_campaigns)
 mycursor.execute(init_cf_users)
 mycursor.execute(init_cf_messages)
-"""
 
-"""
-Series of test inserts
 
-tokensTest = "INSERT INTO cf_tokens (username, expiration) VALUES ("Joe", 1999-04-01);"
-campaignsTest = "INSERT INTO cf_campaigns (GMname) VALUES ("Phil");"
-usersTest = "INSERT INTO cf_users (username, password, color, attributes, GMflag) VALUES ("Fred", "asdfavxdefasdf", "Blue", "testattribute", 0);"
-messagesTest = "INSERT INTO cf_messages(message, username, time, recipient) VALUES ("Hello", "Joe", 19:00:00, "Fred");"
+#Series of test inserts
+tokensTest = 'INSERT INTO cf_tokens (token, username, expiration) VALUES ("abcde12345", "Fred", "1999-04-01");'
+campaignsTest = 'INSERT INTO cf_campaigns (campaignID, GMname) VALUES ("1", "Phil");'
+usersTest = 'INSERT INTO cf_users (campaignID, username, password, color, attributes, GMflag) VALUES ("1", "Fred", "asdfavxdefasdf", "Blue", "testattribute", 0);'
+messagesTest = 'INSERT INTO cf_messages(campaignID, messageID, message, username, time) VALUES ("1", "1", "Hello", "Fred", "19:00:00");'
 
+print("Inserting test data...")
 mycursor.execute(tokensTest)
 mycursor.execute(campaignsTest)
 mycursor.execute(usersTest)
 mycursor.execute(messagesTest)
 
 
-""" 
+
+
 
