193b88f2e810e362662427a6297fc2fd56f209c8
Patrick Bell <patrickb97@hotmail.com>
2019-04-09 11:55:00 -0500

    Changed login and specs

--- mainPage.js
+++ mainPage.js
@@ -48,18 +48,20 @@ function closeFAQ()
 // not done - still need error checking, password hashing, etc.
 function login(){
     var serverAddress = "http://ec2-3-209-137-117.compute-1.amazonaws.com/login/";
+    var campaignID = document.getElementById("campaignID").value.trim();
     var username = document.getElementById("username").value.trim();
     var password = document.getElementById("password").value.trim();
 
     $.ajax({
         url: serverAddress,
         type: 'POST',
-        data:{username: username, passhash: password},
+        data:{username: username, passhash: password, campaignID: campaignID},
         async: true,
         success: function (data) {
             var error = document.getElementById("errorMessage");
             error.innerHTML = ""
             console.log("Post request passed")
+            // Need to set cookie here and redirect
         },
         error: function(data) {
             var error = document.getElementById("errorMessage");
